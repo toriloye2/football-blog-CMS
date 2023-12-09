5a5fbf84ee2c76ce9a40eda86a8b9affeb722393
toriloye2 <140138922+toriloye2@users.noreply.github.com>
2023-12-09 00:43:00 -0600

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,38 @@
+<?php
+
+// Name: oriooooooooooooooooooo
+// Date: 20th Sep. 2023
+// Description:The project is a simple blogging application 
+
+
+
+
+
+// Start the session
+session_start();
+
+// Remove all session variables
+session_unset();
+
+// Destroy the session
+session_destroy();
+echo "You have been logged out!";
+header('Location: login.php');
+exit;
+require('connect.php');
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
