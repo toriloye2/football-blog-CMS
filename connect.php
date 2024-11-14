125f91fb11db9b41bf07637af10a58e9decb81a7
toriloye2 <140138922+toriloye2@users.noreply.github.com>
2024-11-13 22:00:25 -0600

    Fix syntax error by removing extra PHP opening tag

--- connect.php
+++ connect.php
@@ -1,22 +1,4 @@
- <?php
-    //  define('DB_DSN','mysql:host=localhost;dbname=footballside;charset=utf8');
-    //  define('DB_USER','root');
-    //  define('DB_PASS','');
-
-    // //  PDO is PHP Data Objects
-    // //  mysqli <-- BAD.
-    // //  PDO <-- GOOD.
-    //  try {
-    //      // Try creating new PDO connection to MySQL.
-    //      $db = new PDO(DB_DSN, DB_USER, DB_PASS);
-    //      //,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
-    //  } catch (PDOException $e) {
-    //      print "Error: " . $e->getMessage();
-    //      die(); // Force execution to stop on errors.
-    //      // When deploying to production you should handle this
-    //      // situation more gracefully. ¯\_(ツ)_/¯
-    //  }
-    <?php
+<?php
     // Use Heroku's environment variable for DATABASE_URL in production
     $dbUrl = getenv('DATABASE_URL') ?: 'postgres://u6t2brpb6illts:p6a1cfd224349d562a376a40147f19fa4f364bde4d9c102b7b16ff5ec041055d6@c6sfjnr30ch74e.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/df46ec5dt0krj4';
 
@@ -43,5 +25,3 @@
         die();
     }
 ?>
-
-
